b7361b497054edf8692fe334812794ba312a9bc7
ajhwan <129160008+ajhwan@users.noreply.github.com>
2023-04-19 13:18:07 +0900

    Update

--- CountClock_ButtonReset/CountClock_ButtonReset.ino
+++ CountClock_ButtonReset/CountClock_ButtonReset.ino
@@ -1,10 +1,10 @@
-const int interruptPin = 8;//인터럽트핀
-extern volatile unsigned long timer0_millis; //타이머변수
-unsigned long timeVal = 0; //이전시간
-unsigned long millisTime = 0; //현재시간
+const int interruptPin = 8;
+extern volatile unsigned long timer0_millis; 
+unsigned long timeVal = 0;
+unsigned long millisTime = 0; 
 
-int d1, d2, d3, d4;//자리 숫자
-boolean state = false;//타이머 동작 제어 
+int d1, d2, d3, d4;
+boolean state = false;
 
 void setup()
 {
@@ -17,14 +17,14 @@ void setup()
 
 void loop()
 {
-  if(state==true){ //카운트 시작
-    if(millis()-timeVal>=1000){ //1초단위로 시작
+  if(state==true){ 
+    if(millis()-timeVal>=1000){ 
       timeVal=millis();
       millisTime = millis()/1000;
-      d1 = millisTime%10; //1의 자리
-      d2 = (millisTime/10)%10;//10의 자리
-      d3 = (millisTime/100)%10;//100의 자리
-      d4 = (millisTime/1000)%10;//1000의 자리
+      d1 = millisTime%10; 
+      d2 = (millisTime/10)%10;
+      d3 = (millisTime/100)%10;
+      d4 = (millisTime/1000)%10;
 
       Serial.print(d4);
       Serial.print(" : ");
@@ -39,6 +39,6 @@ void loop()
 
 void switchFn(){
   state=!state;
-  timer0_millis=0; //타이머변수 리셋
+  timer0_millis=0;
   timeVal=0;
 }
